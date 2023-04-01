dedc916cf74ad4de187c0411086af501cd8445df
Dan Train <dan.train@red-badger.com>
2023-04-01 19:16:16 +0100

    Use Suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { type NextPage } from "next";
 import { signOut } from "next-auth/react";
+import { Suspense } from "react";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -13,10 +12,9 @@ const Home: NextPage = () => {
           Dan&apos;s Lists
         </h1>
         <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
-          <AuthShowcase />
+          <Suspense fallback={<></>}>
+            <AuthShowcase />
+          </Suspense>
         </div>
       </div>
     </main>
@@ -26,14 +24,11 @@ const Home: NextPage = () => {
 export default Home;
 
 const AuthShowcase = () => {
-  const { data: secretMessage, error } =
-    api.example.getSecretMessage.useQuery();
+  const [secretMessage] = api.example.getSecretMessage.useSuspenseQuery();
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="mb-4 text-center text-2xl text-white">
-        {secretMessage ?? error?.message}
-      </p>
+      <p className="mb-4 text-center text-2xl text-white">{secretMessage}</p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={() => void signOut()}
