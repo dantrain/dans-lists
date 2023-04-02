89d1553b8f6f666cfe7eda2f8e2757a3f5d5af79
Dan Train <dan.train@red-badger.com>
2023-04-02 19:13:00 +0100

    Add delete icon

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,6 +2,19 @@ import { type ReactNode } from "react";
 import { type ListData } from "~/pages";
 import { api } from "~/utils/api";
 
+const DeleteIcon = () => (
+  <svg
+    focusable="false"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    width="16"
+    height="16"
+    fill="currentColor"
+  >
+    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+  </svg>
+);
+
 type ListProps = {
   list: ListData;
   children: ReactNode;
@@ -19,10 +32,11 @@ const List = ({ list: { title, id }, children }: ListProps) => {
       <div className="group mb-2 flex justify-between border-b border-gray-500 pb-1">
         <span className="select-none font-bold">{title}</span>
         <button
-          className="invisible px-2 group-hover:visible"
+          className="invisible px-2 text-gray-400 hover:text-white group-hover:visible"
+          title="Delete"
           onClick={() => deleteList.mutate({ id })}
         >
-          Delete
+          <DeleteIcon />
         </button>
       </div>
       <ul>{children}</ul>
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -11,25 +11,23 @@ const SignIn = ({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] pt-20">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <div className="container flex flex-col items-center justify-center gap-20 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Dan&apos;s Lists
         </h1>
-        <div className="flex flex-col items-center gap-2">
-          <div className="flex flex-col items-center justify-center gap-4">
-            {providers
-              ? Object.values(providers).map((provider) => (
-                  <div key={provider.name}>
-                    <button
-                      className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-                      onClick={() => void signIn(provider.id)}
-                    >
-                      Sign in with {provider.name}
-                    </button>
-                  </div>
-                ))
-              : null}
-          </div>
+        <div className="flex flex-col items-center justify-center gap-4">
+          {providers
+            ? Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                  <button
+                    className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                    onClick={() => void signIn(provider.id)}
+                  >
+                    Sign in with {provider.name}
+                  </button>
+                </div>
+              ))
+            : null}
         </div>
       </div>
     </main>
