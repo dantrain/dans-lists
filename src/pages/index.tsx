6417b9a1b1e168b710ea44a57c4a2dee9d5db5be
Dan Train <dan.train@red-badger.com>
2023-04-02 01:50:50 +0100

    Add checkboxes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { cloneDeep, first, isNil, set } from "lodash";
 import { type NextPage } from "next";
 import { signOut } from "next-auth/react";
@@ -78,28 +79,62 @@ const Lists = () => {
     <>
       <ul className="list-disc text-white">
         {lists.map(({ id, title, items }) => (
-          <li key={id} className="mb-4">
-            <div className="mb-1">{title}</div>
-            <ul className="ml-6 list-disc">
-              {items.map(({ id, title, events }) => (
-                <li key={id}>
-                  <button
-                    onClick={() =>
-                      upsertEvent.mutate({
-                        timezone,
-                        itemId: id,
-                        statusName:
-                          first(events)?.status.name === "COMPLETE"
-                            ? "PENDING"
-                            : "COMPLETE",
-                      })
-                    }
-                  >
-                    {first(events)?.status.name ?? "NULL"}
-                  </button>{" "}
-                  {title}
-                </li>
-              ))}
+          <li key={id} className="mb-5">
+            <div className="mb-2">{title}</div>
+            <ul className="ml-4">
+              {items.map(({ id, title, events }) => {
+                const checked = first(events)?.status.name === "COMPLETE";
+
+                return (
+                  <li key={id} className="mb-2 flex items-center">
+                    <Checkbox.Root
+                      className="cursor-default text-gray-200"
+                      id={id}
+                      checked={checked}
+                      onCheckedChange={() =>
+                        upsertEvent.mutate({
+                          timezone,
+                          itemId: id,
+                          statusName:
+                            first(events)?.status.name === "COMPLETE"
+                              ? "PENDING"
+                              : "COMPLETE",
+                        })
+                      }
+                    >
+                      <Checkbox.Indicator>
+                        <svg
+                          focusable="false"
+                          aria-hidden="true"
+                          viewBox="0 0 24 24"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                        >
+                          <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+                        </svg>
+                      </Checkbox.Indicator>
+                      {!checked && (
+                        <span className="pointer-events-none">
+                          <svg
+                            focusable="false"
+                            aria-hidden="true"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                            fill="currentColor"
+                          >
+                            <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" />
+                          </svg>
+                        </span>
+                      )}
+                    </Checkbox.Root>
+                    <label className="pl-1" htmlFor={id}>
+                      {title}
+                    </label>
+                  </li>
+                );
+              })}
             </ul>
           </li>
         ))}
