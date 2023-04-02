a5fa6e7dd00f32d54a8ce5f3643a29f47f51bbc3
Dan Train <dan.train@red-badger.com>
2023-04-03 00:24:17 +0100

    Use token if it is there

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,12 @@ import withAuth from "next-auth/middleware";
 
 export default withAuth({
   callbacks: {
-    authorized: ({ req }) => {
-      const token =
+    authorized: ({ token, req }) => {
+      return !!(
+        token ||
         req.cookies.get("__Secure-next-auth.session-token") ||
-        req.cookies.get("next-auth.session-token");
-
-      return !!token;
+        req.cookies.get("next-auth.session-token")
+      );
     },
   },
 });
