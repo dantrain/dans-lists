97882004efde9f75b6611632f3ef15ca27fb7fec
Dan Train <dan.train@red-badger.com>
2023-04-03 00:17:47 +0100

    Add post login redirect back

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -37,12 +37,12 @@ const SignIn = ({
 export default SignIn;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  // const session = await getServerSession(context.req, context.res, authOptions);
+  const session = await getServerSession(context.req, context.res, authOptions);
 
   // If the user is already logged in, redirect.
-  // if (session) {
-  //   return { redirect: { destination: "/" } };
-  // }
+  if (session) {
+    return { redirect: { destination: "/" } };
+  }
 
   const providers = await getProviders();
 
