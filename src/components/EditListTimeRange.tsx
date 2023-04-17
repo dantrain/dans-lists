5830e1af0fab0ef11ac5549fd464a053335f0a43
Dan Train <dan.train@red-badger.com>
2023-04-17 19:50:38 +0100

    Add endMinutes input and validation

--- src/components/EditListTimeRange.tsx
+++ src/components/EditListTimeRange.tsx
@@ -1,6 +1,22 @@
-import { cloneDeep, isNil, padStart, range, set } from "lodash";
+import { cloneDeep, isNil, isNull, padStart, range, set } from "lodash";
+import { type SelectHTMLAttributes } from "react";
 import { type ListData } from "~/pages";
 import { api } from "~/utils/api";
+
+const TimeRangeSelect = (props: SelectHTMLAttributes<HTMLSelectElement>) => (
+  <select
+    className="rounded-md border border-[#5b2da0] bg-[#411f72] px-2 py-1"
+    {...props}
+  >
+    <option value="">-</option>
+    {range(96).map((_) => (
+      <option key={_} value={_ * 15}>
+        {((_ / 4) | 0) % 12 || 12}:{padStart(((_ % 4) * 15).toString(), 2, "0")}
+        {(_ / 48) | 0 ? "pm" : "am"}
+      </option>
+    ))}
+  </select>
+);
 
 const EditListTimeRange = ({ list }: { list: ListData }) => {
   const utils = api.useContext();
@@ -15,6 +31,7 @@ const EditListTimeRange = ({ list }: { list: ListData }) => {
 
       if (!isNil(optimisticData) && !isNil(listIndex)) {
         set(optimisticData, [listIndex, "startMinutes"], input.startMinutes);
+        set(optimisticData, [listIndex, "endMinutes"], input.endMinutes);
       }
 
       utils.list.getAll.setData(undefined, optimisticData);
@@ -28,27 +45,37 @@ const EditListTimeRange = ({ list }: { list: ListData }) => {
   });
 
   return (
-    <div className="flex justify-center">
-      <select
-        className="rounded-md border border-[#5b2da0] bg-[#411f72] px-2 py-1"
+    <div className="flex justify-center gap-2">
+      <TimeRangeSelect
         value={list.startMinutes ?? ""}
         onChange={(e) => {
           const startMinutes = parseInt(e.target.value, 10);
           editListTimeRange.mutate({
             id: list.id,
             startMinutes: Number.isNaN(startMinutes) ? null : startMinutes,
+            endMinutes:
+              isNull(list.endMinutes) || list.endMinutes <= startMinutes
+                ? null
+                : list.endMinutes,
           });
         }}
-      >
-        <option value="">-</option>
-        {range(96).map((_) => (
-          <option key={_} value={_ * 15}>
-            {((_ / 4) | 0) % 12 || 12}:
-            {padStart(((_ % 4) * 15).toString(), 2, "0")}
-            {(_ / 48) | 0 ? "pm" : "am"}
-          </option>
-        ))}
-      </select>
+      />
+      <span className="leading-7">&#8211;</span>
+      <TimeRangeSelect
+        className="rounded-md border border-[#5b2da0] bg-[#411f72] px-2 py-1"
+        value={list.endMinutes ?? ""}
+        onChange={(e) => {
+          const endMinutes = parseInt(e.target.value, 10);
+          editListTimeRange.mutate({
+            id: list.id,
+            startMinutes:
+              isNull(list.startMinutes) || list.startMinutes >= endMinutes
+                ? null
+                : list.startMinutes,
+            endMinutes: Number.isNaN(endMinutes) ? null : endMinutes,
+          });
+        }}
+      />
     </div>
   );
 };
--- src/server/api/routers/list.ts
+++ src/server/api/routers/list.ts
@@ -1,3 +1,4 @@
+import { isNull } from "lodash";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { daysOfWeek } from "~/utils/date";
@@ -89,15 +90,26 @@ export const listRouter = createTRPCRouter({
 
   editTimeRange: protectedProcedure
     .input(
-      z.object({
-        id: z.string().cuid(),
-        startMinutes: z.nullable(z.number().int().gte(0).lte(1440)),
-      })
+      z
+        .object({
+          id: z.string().cuid(),
+          startMinutes: z.nullable(z.number().int().gte(0).lte(1440)),
+          endMinutes: z.nullable(z.number().int().gte(0).lte(1440)),
+        })
+        .refine(
+          (_) =>
+            isNull(_.startMinutes) ||
+            isNull(_.endMinutes) ||
+            _.startMinutes <= _.endMinutes
+        )
     )
     .mutation(({ ctx, input }) =>
       ctx.prisma.list.updateMany({
         where: { id: input.id, ownerId: ctx.session.user.id },
-        data: { startMinutes: input.startMinutes },
+        data: {
+          startMinutes: input.startMinutes,
+          endMinutes: input.endMinutes,
+        },
       })
     ),
 
