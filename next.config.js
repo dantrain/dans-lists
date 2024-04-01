5ba396baecb3e0f835b9a689e5454103008ddf04
Dan Train <dan.train@red-badger.com>
2024-04-01 21:18:53 +0100

    Switch to @ducanh2912/next-pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import nextPWA from "next-pwa";
+import nextPWA from "@ducanh2912/next-pwa";
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
@@ -9,7 +9,6 @@ await import("./src/env.js");
 const withPWA = nextPWA({
   dest: "public",
   disable: process.env.NODE_ENV !== "production",
-  buildExcludes: [/middleware-manifest.json$/],
 });
 
 const config = withPWA({});
